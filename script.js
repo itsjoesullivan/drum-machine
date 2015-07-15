98dc395f8898348979d1fe19f30417ae1e54eb9c
itsjoesullivan <itsjoesullivan@gmail.com>
2015-07-15 19:37:18 -0400

    use an audiocontext for timing

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+var context = new AudioContext();
 var drumMachineApp = angular.module("drumMachineApp", []);
 drumMachineApp.controller("RhythmCtrl", function($scope) {
+
+  // stackoverflow.com/questions/12740329/math-functions-in-angular-bindings
+  $scope.Math = Math;
 
   // Where the playhead is
   $scope.cursor = 0;
@@ -8,6 +12,7 @@ drumMachineApp.controller("RhythmCtrl", function($scope) {
   $scope.tempo = 128;
 
   // Process a change to a pattern
+  // TODO: Can ng-model handle this?
   $scope.change = function(sound, index) {
     $scope.rhythm.patterns.some(function(pattern) {
       if (pattern.sound === "sound") {
@@ -24,18 +29,56 @@ drumMachineApp.controller("RhythmCtrl", function($scope) {
 
   // Begin the pattern running
   $scope.play = function() {
-    $scope.pause();
-    $scope.interval = setInterval(function() {
-      $scope.cursor++;
-      $scope.cursor = $scope.cursor % 16;
-      $scope.$apply();
-    }, 1000 * 60 / $scope.tempo / 4);
+    // Remember where we started this play (to keep track of where the
+    // actual cursor is)
+    $scope.startedPlaying = context.currentTime;
+    $scope.offsetAtStart = $scope.cursor % 1;
+    $scope.lastTick = context.currentTime - ($scope.cursor % 1) * $scope.getTickLength();
+    $scope.playing = true;
+    console.log($scope.cursor);
+  };
+
+  $scope.getTickLength = function() {
+    return 60 / $scope.tempo / 4;
   };
 
   // Pause the pattern
   $scope.pause = function() {
-    clearInterval($scope.interval);
+    console.log('offset', $scope.offsetAtStart);
+
+    var tickLength = $scope.getTickLength();
+    // FIXME: this isn't quite right
+    $scope.cursor += ((context.currentTime - $scope.startedPlaying) % tickLength) / tickLength;
+    $scope.cursor = $scope.cursor % 16;
+
+    console.log($scope.cursor);
+
+    $scope.playing = false;
   };
+
+  // Pause and return to beginning
+  $scope.stop = function() {
+    $scope.pause();
+    $scope.cursor = 0;
+  };
+
+  /* Instead of setInterval, run a continuous loop
+   * on requestAnimationFrame, referencing the
+   * (reliable) context.currentTime
+   */
+  $scope.update = function() {
+    if ($scope.playing) {
+      var tickLength = $scope.getTickLength();
+      while (context.currentTime > ($scope.lastTick + tickLength)) {
+        $scope.lastTick = $scope.lastTick + tickLength;
+        $scope.cursor++;
+        $scope.cursor = $scope.cursor % 16;
+        $scope.$apply();
+      }
+    }
+    requestAnimationFrame($scope.update);
+  };
+  $scope.update();
 
   $scope.rhythm = {
     patterns: [
