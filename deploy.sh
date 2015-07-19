2a33cb4a636b786fe003fb5be83918e4a3399eb3
itsjoesullivan <itsjoesullivan@gmail.com>
2015-07-19 13:50:48 -0400

    add actual deploy steps

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+# Run tests
+npm run test
+
+# Move to gh-pages
+git checkout gh-pages
+
+# Checkout new files from master
+git checkout master angular-1.4.3.js
+git checkout master index.html
+git checkout master script.js
+git checkout master kick.wav
+git checkout master snare.wav
+git checkout master hat.wav
+
+git push origin gh-pages
